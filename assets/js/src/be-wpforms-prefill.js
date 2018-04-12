8677b265ec209915f7c070a202a5fe042883686d
Bill Erickson <bill.erickson@gmail.com>
2018-04-12 10:35:35 -0500

    changes

--- assets/js/src/be-wpforms-prefill.js
+++ assets/js/src/be-wpforms-prefill.js
@@ -5,20 +5,8 @@ jQuery(function($){
 	// Get prefilled form data
 	var cookieName = 'be_wpforms_prefill';
 	var formData = Cookies.get( cookieName );
-	if( formData ) {
-		formData = JSON.parse( formData );
-	} else {
-		formData = [];
-	}
-
-	console.log( formData );
-	if( formData.length ) {
-		formData.each( function( index, value ) {
-			if( $('.' + index ).length ) {
-				$('.' + index).val( value );
-			}
-		});
-	}
+	if( ! formData.length )
+		formData = {};
 
 	// Save form data
 	$('.wpforms-container input, .wpforms-container textarea').focusout(function(){
@@ -26,10 +14,9 @@ jQuery(function($){
 		var fieldValue = $(this).val();
 		var formId = $(this).closest('.wpforms-form').attr('id').replace(/\D/g,'');
 		if( $.inArray( formId, prefill.forms ) ) {
-			formData[fieldId] = fieldValue;
+			formData[ fieldId ] = fieldValue;
 			console.log( formData );
-			console.log( JSON.stringify( formData ) );
-			Cookies.set( cookieName, JSON.stringify( formData ), { expires: 365 } );
+			Cookies.set( cookieName, formData, { expires: 365 } );
 		}
 	});
 
